61019e86f2f6d5a91c8d9a1389430757a337a573
David Puelz <dpuelz@gmail.com>
2022-10-04 11:24:20 -0500

    slides update

--- code/bootstrap.R
+++ code/bootstrap.R
@@ -44,6 +44,47 @@ hist(boot1$age)
 # bootstrapped standard errors
 sd(boot1$Intercept)
 sd(boot1$age)
+summary(lm1)
 
 # confidence intervals
 confint(boot1)
+
+
+# Going back to power demand
+# What about an additional variable?
+KHOU_squared = load_combined$KHOU^2
+load_combined$KHOU_squared = KHOU_squared
+numboot = 200
+lm2 = do(numboot)*(lm(COAST ~ KHOU + KHOU_squared, data=resample(load_combined)))
+
+plot(load_combined$KHOU,load_combined$COAST,col=rgb(0,0,0,alpha=0.1),pch=19,cex=0.8)
+x = load_combined$KHOU
+xmin = floor(min(x))
+xmax = floor(max(x))
+xnew = seq(xmin,xmax,length.out=500)
+quadfit = array(NA,c(numboot,length(xnew)))
+for(ii in 1:numboot){
+  quadfit[ii,] = lm2$Intercept[ii] + lm2$KHOU[ii]*xnew +   lm2$KHOU_squared[ii]*xnew^2
+  lines(xnew,quadfit[ii,],col=rgb(1,0,0,alpha=0.3),lwd=1)
+}
+
+# smaller sample
+load_combined_small = sample(load_combined,500)
+KHOU_squared = load_combined_small$KHOU^2
+load_combined_small$KHOU_squared = KHOU_squared
+numboot = 200
+lm2 = do(numboot)*(lm(COAST ~ KHOU + KHOU_squared, data=resample(load_combined_small)))
+
+plot(load_combined_small$KHOU,load_combined_small$COAST,col=rgb(0,0,0,alpha=0.1),pch=19,cex=0.8)
+x = load_combined_small$KHOU
+xmin = floor(min(x))
+xmax = floor(max(x))
+xnew = seq(xmin,xmax,length.out=500)
+quadfit = array(NA,c(numboot,length(xnew)))
+for(ii in 1:numboot){
+  quadfit[ii,] = lm2$Intercept[ii] + lm2$KHOU[ii]*xnew +   lm2$KHOU_squared[ii]*xnew^2
+  lines(xnew,quadfit[ii,],col=rgb(1,0,0,alpha=0.3),lwd=1)
+}
+
+
+
