ccd3543ad8d4c567b6bf9e10239738e29e2e51e4
David Puelz <dpuelz@gmail.com>
2022-11-17 15:02:54 -0600

    randomizer for debate

--- code/student_randomizer.R
+++ code/student_randomizer.R
@@ -2,9 +2,8 @@
 
 set.seed(1)
 
-groups = read.csv("../data/project_groups_2022.csv",header = T)
-ord = sample(1:nrow(groups))
+students = read.csv("../data/students_2022.csv",header = F)
+ord = sample(1:nrow(students))
 
-final = cbind(c('Tues','Tues','Tues','Thurs','Thurs'),groups[ord,])
-colnames(final) = ''
-final
+final = cbind(noquote(c(rep('JPE',6),rep('Walmart',6))),students[ord,])
+noquote(final)
