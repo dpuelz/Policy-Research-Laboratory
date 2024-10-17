a7064a8964b87c0d85b87aa8c6c9fd4087c4df1e
David Puelz <dpuelz@gmail.com>
2024-10-17 07:36:42 -0500

    Update hclust_examples.R

--- code/hclust_examples.R
+++ code/hclust_examples.R
@@ -6,7 +6,7 @@ protein_scaled = scale(protein, center=TRUE, scale=TRUE)
 
 # Form a pairwise distance matrix using the dist function
 protein_distance_matrix = dist(protein_scaled)
-protein_distance_matrix = dist(protein_scaled, method='manhattan')
+# protein_distance_matrix = dist(protein_scaled, method='manhattan')
 
 
 # Now run hierarchical clustering
@@ -57,11 +57,11 @@ sigma = attr(X,"scaled:scale")
 distance_between_cars = dist(X)
 
 # Now run hierarchical clustering
-h1 = hclust(distance_between_cars, method='complete')
+h1 = hclust(distance_between_cars, method='average')
 plot(h1, cex=0.3)
 
 # Cut the tree into 10 clusters
-cluster1 = cutree(h1, k=10)
+cluster1 = cutree(h1, k=2)
 summary(factor(cluster1))
 
 # Examine the cluster members
@@ -72,14 +72,14 @@ plot(h1, cex=0.3)
 
 
 # Now run hierarchical clustering
-h1 = hclust(distance_between_cars, method='single')
+h1 = hclust(distance_between_cars, method='average')
 
 # Cut the tree into 10 clusters
 cluster1 = cutree(h1, k=10)
 summary(factor(cluster1))
 
 # Examine the cluster members
-which(cluster1 == 9)
+which(cluster1 == 3)
 
 # Plot the dendrogram
 plot(h1, cex=0.3)
